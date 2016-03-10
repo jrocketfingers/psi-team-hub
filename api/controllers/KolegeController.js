1d8a6601cbcfb717a703bef0475552e4aa8e1ad0
Nikola Kolevski <nikola.kolevski@borneagency.com>
2016-03-10 20:03:49 +0100

    Initial commit. (sails)

---
+++ api/controllers/KolegeController.js
@@ -0,0 +1,11 @@
+/**
+ * KolegeController
+ *
+ * @description :: Server-side logic for managing koleges
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
