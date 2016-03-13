e3546ecc2d732138bcec2f689e62b4debff6a84a
Johnny <developer94@gmail.com>
2016-03-13 16:30:33 +0100

    Add api endpoints.

--- api/models/Role.js
+++ api/models/Role.js
@@ -1,5 +1,5 @@
 /**
- * User.js
+ * Role.js
  *
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
@@ -8,25 +8,15 @@
 module.exports = {
 
   attributes: {
-    username: {
+    name: {
       type: 'string',
       unique: true
     },
 
-    indeks: {
-      type: 'string',
-      unique: true
-    },
-
-    roles: {
-      collection: 'Role',
-      via: 'users'
-    },
-
-    //skills: {
-      //collection: 'Skill',
-      //via: 'skills'
-    //}
+    users: {
+      collection: 'User',
+      via: 'roles',
+    }
   }
 };
 
