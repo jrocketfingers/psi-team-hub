00af53eb016cfe56a3ad9569eafea2a0a36bff0f
simke9445 <simke9445@gmail.com>
2016-03-14 14:29:51 +0100

    add fade-in & fade-out transition on navigation

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,3 +4,27 @@ $('.ui.button.primary').on('click', function(){
   $('.ui.modal').modal('show');
 
 });
+
+
+$(document).ready(function() {
+
+    $('#main-content').css('display', 'none');
+
+    $('#main-content').fadeIn(500);
+
+    $('a.switch').click(function(event) {
+
+        event.preventDefault();
+
+        newLocation = this.href;
+
+        $('#main-content').fadeOut(500, newpage);
+    });
+
+    function newpage() {
+
+        window.location = newLocation;
+
+    }
+
+});
