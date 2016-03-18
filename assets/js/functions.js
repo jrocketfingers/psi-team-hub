318d93272a1c74651be0150550702801dd929409
simke9445 <simke9445@gmail.com>
2016-03-18 16:04:47 +0100

    team & student modal action definitions

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,9 +4,49 @@ $('#login').on('click', function(){
 
 });
 
+$('.team.subscribe').on('click', function(){
+
+  $('#team-modal').modal('show');
+
+});
+
+$('#subscribe-accept').on('click', function(){
+
+  //temporary
+  $('#team-modal').modal('hide');
+
+});
+
+$('#subscribe-reject').on('click', function(){
+
+  //temporary
+  $('#team-modal').modal('hide');
+
+});
+
 $('#register').on('click',function(){
 
   $('#register-modal').modal('show');
+
+});
+
+$('.call.student').on('click', function(){
+
+  $('#student-modal').modal('show');
+
+});
+
+$('#call-accept').on('click', function(){
+
+  //temporary
+  $('#student-modal').modal('hide');
+
+});
+
+$('#call-reject').on('click', function(){
+
+  //temporary
+  $('#student-modal').modal('hide');
 
 });
 
